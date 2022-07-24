04828fa567c88f904eb0ce0a8878d37cbb21ba63
NGUYEN GIANG NAM <kow.giangnam@gmail.com>
2022-07-24 11:57:38 +0700

    first commit

---
+++ register.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+</head>
+<body>
+    <div>
+        <h3>login</h3>
+        <form method="POST">
+            <label>Username</label>&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="text" name="username" placeholder="username">
+            <br><br>
+            <label>Password</label>&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="password" name="password" placeholder="password"> 
+            <label>Confirm password</label>&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="password" name="cfpassword" placeholder="confirm password"> 
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="submit" name="login" value="login"> 
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="http://192.168.241.130/register.php">register?</a>
+        </form>
+</body>
+</html>
